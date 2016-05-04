39e5a1338515696956b29e0b41eefd6cbab24d6a
Kevin Brightwell <kevin.brightwell2@gmail.com>
2016-05-04 12:37:39 -0400

    Fix conditional finding

--- index.js
+++ index.js
@@ -2,35 +2,48 @@
 
 const assert = require('assert');
 const fs = require('fs');
+const path = require('path');
 
 const cfenv = require('cfenv');
 const _ = require('lodash');
 
-module.exports = function Config(path) {
+module.exports = function Config(pathname) {
 
-  assert(_.isString(path), "Must specify path that exists.");
+  assert(_.isString(pathname), "Must specify pathname that exists.");
   
-  fs.accessSync(path, fs.R_OK);
+  fs.accessSync(pathname, fs.R_OK);
 
   let vcap;
   let config;
 
   if (!!process.env['VCAP_APP_PORT']) {
     // bluemix
-    config = require(path + '/config.bluemix.json');
+    config = require(path.join(pathname, 'config.bluemix.json'));
 
     vcap = cfenv.getAppEnv();
-    _.each(config.vcap.services, (v, key) => {
-      vcap.services[key] = v;
-    });
+    
+    if (_.has(config, 'vcap.services')) {
+      _.each(config.vcap.services, (v, key) => {
+        vcap.services[key] = v;
+      });  
+    }
   } else {
-
-    config = require(path + '/config.local.json');
-
-    vcap = cfenv.getAppEnv({ vcap: config.vcap });
+    // local 
+    config = require(path.join(pathname, 'config.local.json'));
+    
+    let ext = {};
+    if (_.has(config, 'vcap')) {
+      ext = { vcap: config.vcap };
+    }
+    
+    vcap = cfenv.getAppEnv(ext);
   }
 
-  vcap["locals"] = config.locals;
+  if (_.has(vcap, "locals")) {
+    vcap.locals = config.locals;
+  } else {
+    vcap.locals = {};
+  }
 
   return vcap;
 };
